72108abc8d62872c626a020543a84bea768eaa00
SanyakornPFP <sanyakorn-it@pfp-pacific.com>
2025-05-23 17:42:12 +0700

    Update

--- WorkPermitManager/obj/Debug/net9.0/WorkPermitManager.AssemblyInfo.cs
+++ WorkPermitManager/obj/Debug/net9.0/WorkPermitManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkPermitManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7480f562759b86cbd02082de727799e911dc0da2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a500d7523032805794d4f079b394cb3bd4133e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkPermitManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkPermitManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
